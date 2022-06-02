ca906aa9cc5a216726364ed99fa7bdc56c3ddc1c
ardakozann <ardakozan97@gmail.com>
2022-06-03 00:27:15 +0300

    clean code tekniği uygulandı

--- template.js
+++ template.js
@@ -3,18 +3,14 @@ number=prompt("Sayı giriniz");
 document.write("Verilen sayı : "+number+'<br>');
 if(isNumberSecretKey(number) && checkIfNumberUnder1(number)){
     howManyNumbers(number);
-    plusAllNumber(number);
     oddNumber(number);
-    plusOddNumber(number);
-    showOddNumber(number);
     evenNumber(number);
-    plusEvenNumber(number);
-    showEvenNumber(number);
 }
 
 //1.adım: kaç tane sayı var ?
 function howManyNumbers(number){
     var numberOfNumber=0;
+    var plus=0;
     for (var i=1; i <= number; i++) {
         if(checkIfExist7(i)){
             continue;
@@ -23,26 +19,16 @@ function howManyNumbers(number){
             break;
         }
         numberOfNumber++;
-    }
-    document.write("Sayı adeti : "+numberOfNumber+'<br>');
-}
-//2.adım: sayı toplamları var ?
-function plusAllNumber(number) {
-    var plus=0;
-    for(var i=1 ; i<=number ; i++){
-        if(checkIfExist7(i)){     
-            continue;
-        }
-        if(checkIfMore100(i)){
-            break;
-        }
         plus+=i;
     }
+    document.write("Sayı adeti : "+numberOfNumber+'<br>');
     document.write("Sayıların toplamı : "+plus+'<br>');
 }
 //3.adım: kaç tane tek  sayı var ?
 function oddNumber(number){
     var oddNumberOfNumber=0;
+    var plusOdd=0;
+    document.write("Tek sayılar : 0");
     for(var i=1; i <= number; i++){
         if(i%2==0 || checkIfExist7(i)){
             continue;
@@ -51,40 +37,17 @@ function oddNumber(number){
             break;
         }
         oddNumberOfNumber++;
-    }
-    document.write("Tek sayı adeti : "+oddNumberOfNumber+'<br>');
-}
-//4.adım: tek sayılar toplamı ?
-function plusOddNumber(number){
-    var plusOdd=0;
-    for(var i=1 ; i<=number ; i++){
-        if(i%2==0 || checkIfExist7(i)){
-            continue;
-        }
-        if(checkIfMore100(i)){
-            break;
-        }
         plusOdd+=i;
-    }
-    document.write("Tek sayıların toplamı : "+plusOdd+'<br>');
-}
-//5.adım: tek sayılar gösterelim ?
-function showOddNumber(number){
-    document.write("Tek sayılar : 0");
-    for(var i=1; i<=number; i++){
-        if(i%2==0 || checkIfExist7(i)){
-            continue;
-        }
-        if(checkIfMore100(i)){
-            break;
-        }
         document.write("-"+i);
     }
-    document.write('<br>');  
+    document.write('<br>'+"Tek sayı adeti : "+oddNumberOfNumber);
+    document.write('<br>'+"Tek sayıların toplamı : "+plusOdd+'<br>');
 }
 //6.adım: kaç tane çift  sayı var ?
 function evenNumber(number){
     var evenNumberOfNumber=0;
+    var plusEven=0;
+    document.write("Çift sayılar : 0");
     for(var i=1; i <= number; i++){
         if(i%2==1){
             continue;
@@ -92,37 +55,12 @@ function evenNumber(number){
         if(checkIfMore100(i)){
             break;
         }
+        document.write("-"+i);
         evenNumberOfNumber++;
-    }
-    document.write("Çift sayı adeti : "+evenNumberOfNumber+'<br>');
-}
-//7.adım: çift sayılar toplamı ?
-function plusEvenNumber(number){
-    var plusEven=0;
-    for(var i=1 ; i<=number ; i++){
-        if(i%2==1){
-            continue;
-        }
-        if(checkIfMore100(i)){
-            break;
-        }
         plusEven+=i;
     }
-    document.write("Çift sayıların toplamı : "+plusEven+'<br>');
-}
-//8.adım: çift sayılar gösterelim ?
-function showEvenNumber(number){
-    document.write("Çift sayılar : 0");
-    for(var i=1; i<=number; i++){
-        if(i%2==1 ){
-            continue;
-        }
-        if(checkIfMore100(i)){
-            break;
-        }
-        document.write("-"+i);
-    }
-    document.write('<br>');  
+    document.write('<br>'+"Çift sayı adeti : "+evenNumberOfNumber);
+    document.write('<br>'+"Çift sayıların toplamı : "+plusEven);
 }
 //Eğer verilen sayılarda 7 sayısı varsa bunu eklemesin (continue)
 function checkIfExist7(number){
